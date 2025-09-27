97564e924953c2092993ee6bee9c9f41b0c9b5e4
AnnaBielska <anna.bielska@sysgears.com>
2025-09-27 22:45:17 +0300

    Added basic colors & typography to Tailwind theme.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>Hello, test task</>
+    <main className="p-8 space-y-4">
+      <h1 className="text-h1">H1 — 48px</h1>
+      <h2 className="text-h2">H2 — 40px</h2>
+      <h3 className="text-h3">H3 — 32px</h3>
+      <h4 className="text-h4">H4 — 24px</h4>
+      <h5 className="text-h5">H5 — 20px</h5>
+      <h6 className="text-h6">H6 — 18px</h6>
+
+      <p className="text-subtitle1">Subtitle1 — 18px</p>
+      <p className="text-subtitle2">Subtitle2 — 14px</p>
+
+      <p className="text-body1">Body1 — 16px</p>
+      <p className="text-body2">Body2 — 14px</p>
+
+      <span className="text-caption">Caption — 12px</span>
+      <span className="text-overline">Overline — 12px uppercase</span>
+    </main>
   );
 }
