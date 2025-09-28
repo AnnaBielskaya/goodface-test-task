c1ffdcdd474345c8ecfa0b21d109d02e5aa9a770
AnnaBielska <anna.bielska@sysgears.com>
2025-09-28 20:32:22 +0300

    Reused Plan Labels component.

--- src/components/all-products/Plans.tsx
+++ src/components/all-products/Plans.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Image from "next/image";
+import { PlanLabel } from "@/ui/PlanLabel";
 
 const plans = [
   "Unlimited Concurrent Sessions",
@@ -19,15 +19,7 @@ export default function Plans() {
       <h2 className="text-h6 mb-4 text-grey-800">All plans include</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-y-2">
         {plans.map((feature, index) => (
-          <div key={index} className="flex items-center gap-2">
-            <Image
-              src="/icons/circle-check-filled.svg"
-              alt="All products logo"
-              width={20}
-              height={20}
-            />
-            <span className="text-body2 text-grey-600">{feature}</span>
-          </div>
+          <PlanLabel key={index} text={feature} />
         ))}
       </div>
     </div>
