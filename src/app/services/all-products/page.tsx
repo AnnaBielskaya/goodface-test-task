0a67dd5cb6d4c38dae13dca77a59b54d736b38f9
AnnaBielska <anna.bielska@sysgears.com>
2025-09-28 09:10:06 +0300

    Added All Products page.

---
+++ src/app/services/all-products/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">All products content</h1>
+      <p className="mt-2">Coming soon...</p>
+    </div>
+  );
+}
