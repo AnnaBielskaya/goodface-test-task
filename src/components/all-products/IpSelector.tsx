5a7cf39376f2808c882833bb3057eac58fc15613
AnnaBielska <anna.bielska@sysgears.com>
2025-09-29 01:48:28 +0300

    Customized IP slider.

--- src/components/all-products/IpSelector.tsx
+++ src/components/all-products/IpSelector.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/ui/Button";
 import { ChevronUp } from "lucide-react";
 import { useState } from "react";
 import { EditIcon } from "@/assets/icons/EditIcon";
+import IpSlider from "./IpSlider";
 
 const bundleDiscounts = [
   { range: "10-24", price: "3.00" },
@@ -23,7 +24,6 @@ export default function IpSelector() {
           Choose the perfect quantity of IPs for your needs effortlessly
         </p>
       </div>
-
       <div>
         <button
           className="flex flex-row gap-1 items-center cursor-pointer"
@@ -65,63 +65,20 @@ export default function IpSelector() {
             ))}
           </div>
         )}
-
-        <IpSlider />
       </div>
 
-      <div></div>
+      <IpSlider />
 
       <Button
         className="w-fit text-brand-500"
         icon={<EditIcon />}
         label="Enter a custom quantity"
+      />{" "}
+
+      <Button
+        className="w-fit text-brand-500"
+        label="Select from the range"
       />
-    </div>
-  );
-}
-
-function IpSlider() {
-  const [ipCount, setIpCount] = useState(100);
-  const min = 10;
-  const max = 1000;
-
-  const progress = ((ipCount - min) / (max - min)) * 100;
-
-  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIpCount(Number(event.target.value));
-  };
-
-  const labels = [10, 50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
-
-  return (
-    <div className="relative w-full py-10">
-      <div className="relative h-2 w-full">
-        <div className="absolute top-0 left-0 h-full w-full rounded-full bg-grey-100"></div>
-
-        <div
-          className="absolute top-0 left-0 h-full rounded-full bg-brand-400"
-          style={{
-            width: progress >= 100 ? "100%" : `${progress}%`,
-          }}
-        ></div>
-
-        <input
-          type="range"
-          min={min}
-          max={max}
-          value={ipCount}
-          onChange={handleSliderChange}
-          className="custom-range-slider absolute top-0 left-0 h-full w-full cursor-pointer appearance-none bg-transparent"
-        />
-      </div>
-
-      <div className="absolute left-0 mb-2 flex w-full justify-between translate-y-full">
-        {labels.map((label) => (
-          <span key={label} className="text-center text-xs text-grey-500">
-            {label}
-          </span>
-        ))}
-      </div>
     </div>
   );
 }
