d74f9d03165ad5d07f56883613c611cba49f0674
AnnaBielska <anna.bielska@sysgears.com>
2025-09-29 18:18:24 +0300

    Updated typography.

--- src/components/all-products/IpSelector.tsx
+++ src/components/all-products/IpSelector.tsx
@@ -69,8 +69,8 @@ export default function IpSelector({ value, onValueChange }: IpSelectorProps) {
   return (
     <div className="flex flex-col gap-6">
       <div>
-        <p className="text-subtitle2 text-grey-800">Select number of IPs</p>
-        <p className="text-body-2 text-grey-500">
+        <p className="text-label text-grey-800">Select number of IPs</p>
+        <p className="text-body2 text-grey-500">
           Choose the perfect quantity of IPs for your needs effortlessly
         </p>
       </div>
