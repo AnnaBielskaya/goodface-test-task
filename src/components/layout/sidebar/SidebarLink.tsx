a1ae8d65a4fdaa80eb7e541f0412334451a0ed0d
AnnaBielska <anna.bielska@sysgears.com>
2025-09-30 09:07:19 +0300

    Updated icons color.

--- src/components/layout/sidebar/SidebarLink.tsx
+++ src/components/layout/sidebar/SidebarLink.tsx
@@ -2,24 +2,45 @@ import type { LinkItem } from "./sidebar.config";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function SidebarLink({ href, icon: Icon, endIcon: EndIcon, label }: LinkItem) {
-    const pathname = usePathname();
-    const isActive = pathname === href;
-  
-    return (
-      <Link
-        href={href}
-        className={`sidebar-link text-subtitle2 transition-colors ${
-          isActive
-            ? "bg-brand-50 text-brand-500"
-            : "text-grey-800 hover:bg-brand-50 hover:text-brand-500"
-        }`}
-      >
-        <div className="flex items-center gap-4">
-          {Icon && <Icon className="sidebar-icon" />}
-          <span className="text-subtitle2">{label}</span>
-        </div>
-        {EndIcon && <EndIcon className="h-4 w-4 text-brand-500" />}
-      </Link>
-    );
-  }
+export default function SidebarLink({
+  href,
+  icon: Icon,
+  endIcon: EndIcon,
+  label,
+}: LinkItem) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <Link
+      href={href}
+      className={`sidebar-link group text-subtitle2 transition-colors ${
+        isActive
+          ? "bg-brand-50 text-brand-500"
+          : "text-grey-800 hover:bg-brand-50 hover:text-brand-500"
+      }`}
+    >
+      <div className="flex items-center gap-4">
+        {Icon && (
+          <Icon
+            className={`sidebar-icon transition-colors ${
+              isActive
+                ? "text-brand-500"
+                : "text-grey-700 group-hover:text-brand-500"
+            }`}
+          />
+        )}
+        <span className="text-subtitle2">{label}</span>
+      </div>
+      {EndIcon && (
+        <EndIcon
+          className={`h-4 w-4 transition-colors ${
+            isActive
+              ? "text-brand-500"
+              : "text-grey-700 group-hover:text-brand-500"
+          }`}
+        />
+      )}
+    </Link>
+  );
+}
